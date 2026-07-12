2c7a2439186824d93ca294cc4a73834a9274897c
Florararie <samueltayloratkinson1@gmail.com>
2026-07-12 18:01:38 -0400

    Allow barriers to be affected by pistons

--- src/main/java/floramene/barrierbreak/mixin/BlockBehaviourPropertiesMixin.java
+++ src/main/java/floramene/barrierbreak/mixin/BlockBehaviourPropertiesMixin.java
@@ -3,6 +3,7 @@ package floramene.barrierbreak.mixin;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.material.PushReaction;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -16,14 +17,19 @@ public abstract class BlockBehaviourPropertiesMixin {
     @Shadow
     public abstract BlockBehaviour.Properties strength(float destroyTime, float explosionResistance);
 
+    @Shadow
+    public abstract BlockBehaviour.Properties pushReaction(PushReaction pushReaction);
+
     @Inject(
         method = "setId",
         at = @At("RETURN")
     )
     public void makeBarrierInstaBreak(ResourceKey<Block> resourceKey, CallbackInfoReturnable<BlockBehaviour.Properties> cir) {
         Identifier id = resourceKey.identifier();
+
         if (id.getNamespace().equals("minecraft") && id.getPath().equals("barrier")) {
             this.strength(0.0F, 3600000.0F);
+            this.pushReaction(PushReaction.NORMAL);
         }
     }
 }
